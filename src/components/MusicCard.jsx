2005f5df5b800362cd89ce78292f84b80ccbf5bb
Arthur Vianna <arthurpedrosa96.ap@gmail.com>
2022-07-04 16:33:20 -0300

    created favorites page

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,24 +1,39 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
 
+import './MusicCard.css';
+
 class MusicCard extends Component {
   render() {
-    const { track, isSongFavorite, callback } = this.props;
-    const { trackId, trackName, previewUrl } = track;
+    const { track, isSongFavorite, callback, albumCover = null } = this.props;
+    const { trackId, trackName, previewUrl, collectionName, artistName } = track;
     return (
       <div key={ trackId } className="track">
         <div className="track-name-favorite">
-          <p>{trackName}</p>
+          {albumCover && <img src={ albumCover } alt="album cover" />}
+          <div className="track-name">
+            <h4>{trackName}</h4>
+            {albumCover && (
+              <>
+                <p>{collectionName}</p>
+                <p>{artistName}</p>
+              </>
+            )}
+          </div>
           <div className="checkbox-container">
             {isSongFavorite
               ? <i className="fa-solid fa-heart" />
               : <i className="fa-regular fa-heart" />}
-            <input
-              type="checkbox"
-              checked={ isSongFavorite }
-              data-testid={ `checkbox-music-${trackId}` }
-              onChange={ () => callback(track) }
-            />
+            <label htmlFor={ trackId }>
+              Favorita
+              <input
+                id={ trackId }
+                type="checkbox"
+                checked={ isSongFavorite }
+                data-testid={ `checkbox-music-${trackId}` }
+                onChange={ () => callback(track) }
+              />
+            </label>
           </div>
         </div>
         <audio
@@ -38,9 +53,12 @@ MusicCard.propTypes = {
     trackId: propTypes.number.isRequired,
     trackName: propTypes.string.isRequired,
     previewUrl: propTypes.string.isRequired,
+    collectionName: propTypes.string.isRequired,
+    artistName: propTypes.string.isRequired,
   }).isRequired,
   isSongFavorite: propTypes.bool.isRequired,
   callback: propTypes.func.isRequired,
+  albumCover: propTypes.string.isRequired,
 };
 
 export default MusicCard;
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
+
+import './Favorites.css';
+import { getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
 
 class Favorites extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+      favoriteSongs: [],
+    };
+  }
+
+  async componentDidMount() {
+    const favoriteSongs = await getFavoriteSongs();
+    this.setState({ favoriteSongs, isLoading: false });
+  }
+
+  async removeTrackFromFavorites(track) {
+    this.setState({ isLoading: true });
+    await removeSong(track);
+    const favoriteSongs = await getFavoriteSongs();
+    this.setState({ favoriteSongs, isLoading: false });
+  }
+
   render() {
+    const { isLoading, favoriteSongs } = this.state;
     return (
-      <div data-testid="page-favorites">
+      <div className="favorites-page" data-testid="page-favorites">
         <Header />
-        Favorites
+        {isLoading
+          ? <Loading />
+          : (
+            <section className="section__favorites">
+              <h3>Músicas Favoritas</h3>
+              {favoriteSongs.map((song) => (
+                <MusicCard
+                  key={ song.trackId }
+                  track={ song }
+                  isSongFavorite
+                  callback={ () => this.removeTrackFromFavorites(song) }
+                  albumCover={ song.artworkUrl100 }
+                />))}
+            </section>
+          )}
       </div>);
   }
 }
