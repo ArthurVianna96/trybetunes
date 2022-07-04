070c6ade5cc3b6f3f984b50ca9053d4b62fb653b
Arthur Vianna <arthurpedrosa96.ap@gmail.com>
2022-07-04 10:33:22 -0300

    created searchBar por search page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,51 @@
 import React, { Component } from 'react';
+
+import './Search.css';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchQuery: '',
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange({ target: { value } }) {
+    this.setState({ searchQuery: value });
+  }
+
+  isInputValid(input) {
+    const minInputLength = 2;
+    return input.length < minInputLength;
+  }
+
   render() {
+    const { searchQuery } = this.state;
+    const isButtonDisabled = this.isInputValid(searchQuery);
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <section className="section__search">
+          <div className="search-bar">
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              placeholder="Nome do artista"
+              value={ searchQuery }
+              onChange={ this.handleInputChange }
+            />
+            <i className="fa-solid fa-magnifying-glass" />
+          </div>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ isButtonDisabled }
+          >
+            Pesquisar
+          </button>
+        </section>
       </div>);
   }
 }
