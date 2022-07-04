d3d5e2bd2146a9eeea693c348b4ad1110d790b09
Arthur Vianna <arthurpedrosa96.ap@gmail.com>
2022-07-04 15:57:51 -0300

    login function refactor

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import { createUser, getUser } from '../services/userAPI';
+import { createUser } from '../services/userAPI';
 import Loading from '../components/Loading';
 
 class Login extends Component {
